1e0d4fe42b803cf92f9031a925151330f42ec2b4
flimblewitz <9010741+flimblewitz@users.noreply.github.com>
2022-10-30 20:54:36 -0400

    I had to crib an existing solution, but at least I understand now. I undervalued the benefit of sorting: I could have just kept skipping i and k values that resulted in duplicate combinations, thereby eliminating the need for a set

--- three_sum/src/main.rs
+++ three_sum/src/main.rs
@@ -1,115 +1,51 @@
 fn main() {
-    // println!("{:?}", Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]));
+    println!("{:?}", Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]));
     println!(
         "{:?}",
         Solution::three_sum(vec![-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0])
     );
-    // println!("{:?}", Solution::three_sum(vec![1, 1, -2]));
+    println!("{:?}", Solution::three_sum(vec![1, 1, -2]));
 }
 
 struct Solution {}
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut nums = nums.clone();
+        let mut nums = nums;
         nums.sort();
-        let index_of_first_non_negative_or_last_index = nums
-            .iter()
-            .enumerate()
-            .find(|(_, val)| **val >= 0)
-            .unwrap_or((nums.len() - 1, &0))
-            .0;
-        let mut tuple_set = std::collections::HashSet::new();
-        for i in 0..(nums.len() - 2) {
-            if nums[i] > 0 {
-                break;
+        let mut res = vec![];
+
+        for i in 0..nums.len() {
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue;
             }
-            for j in (i + 1)..(nums.len() - 1) {
-                for k in (j + 1).max(index_of_first_non_negative_or_last_index)..(nums.len()) {
-                    if nums[k] < 0 {
-                        continue;
-                    }
-                    if nums[i] + nums[j] + nums[k] == 0 {
-                        let mut v = vec![nums[i], nums[j], nums[k]];
-                        v.sort();
-                        tuple_set.insert((v[0], v[1], v[2]));
-                    }
+            let mut j = i + 1;
+            let mut k = nums.len() - 1;
+
+            while j < k {
+                // after the first values of j and k, let's avoid all repeated values of num[j] and num[k]
+                if j > i + 1 && nums[j] == nums[j - 1] {
+                    j += 1;
+                    continue;
+                }
+                if k < nums.len() - 1 && nums[k] == nums[k + 1] {
+                    k -= 1;
+                    continue;
+                }
+
+                let sum = nums[i] + nums[j] + nums[k];
+                if sum == 0 {
+                    // if there's a match, congratulations. But there might still be possible values of j and k that result in a sum of 0, so tighten the inner window and try again
+                    res.push([nums[i], nums[j], nums[k]].to_vec());
+                    j += 1;
+                    k -= 1;
+                } else if sum < 0 {
+                    j += 1;
+                } else {
+                    k -= 1;
                 }
             }
         }
-        tuple_set.iter().map(|z| vec![z.0, z.1, z.2]).collect()
+
+        res
     }
 }
-
-// the above works, but the time limit gets exceeded
-
-// impl Solution {
-//     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-//         let mut nums: Vec<i32> = nums.iter().cloned().collect();
-//         nums.sort();
-//         // println!("length: {}, {nums:?}", nums.len());
-
-//         // gotta sort
-//         // gotta iterate i over all negatives and non-positives
-//         // gotta iterate j over everything between i and whatever nums.len()-1
-//         // gotta iterate k over all non-negatives
-
-//         let last_i_exclusive = nums
-//             .iter()
-//             .enumerate()
-//             .filter(|(_index, val)| **val <= 0)
-//             .rev()
-//             .next()
-//             .unwrap_or((1, &0))
-//             .0
-//             .min(nums.len() - 3)
-//             + 1;
-
-//         let first_k = nums
-//             .iter()
-//             .enumerate()
-//             .filter(|(_index, val)| **val >= 0)
-//             .next()
-//             .unwrap_or((nums.len() - 1, &0))
-//             .0;
-
-//         // let index_of_first_positive = nums
-//         //     .iter()
-//         //     .enumerate()
-//         //     .skip(1)
-//         //     // .inspect(|(index, val)| println!("{index}, {val}"))
-//         //     .find(|(index, val)| nums[*index - 1] <= 0 && **val > 0)
-//         //     .unwrap_or((nums.len(), &0))
-//         //     .0;
-//         // let index_of_last_nonpositive = if index_of_first_positive == nums.len() {
-//         //     index_of_first_positive - 1
-//         // } else {
-//         //     nums.len() - 1
-//         // };
-
-//         // let index_of_first_k = index_of_first_positive.min(index_of_last_nonpositive);
-//         // println!("{index_of_first_positive}, {index_of_last_nonpositive}, {index_of_first_k}");
-
-//         // println!(
-//         //     "len: {}, index_of_first_positive: {}",
-//         //     nums.len(),
-//         //     index_of_first_positive
-//         // );
-
-//         let mut tuple_set = std::collections::HashSet::new();
-//         // for i in 0..(index_of_first_k - 1) {
-//         for i in 0..last_i_exclusive {
-//             for j in (i + 1)..(nums.len() - 1) {
-//                 // for k in (j + 1).max(index_of_first_positive)..(nums.len()) {
-//                 for k in (j + 1).max(first_k)..(nums.len()) {
-//                     // println!("{i}, {j}, {k}");
-//                     if nums[i] + nums[j] + nums[k] == 0 {
-//                         let mut v = vec![nums[i], nums[j], nums[k]];
-//                         v.sort();
-//                         tuple_set.insert((v[0], v[1], v[2]));
-//                     }
-//                 }
-//             }
-//         }
-//         tuple_set.iter().map(|z| vec![z.0, z.1, z.2]).collect()
-//     }
-// }
